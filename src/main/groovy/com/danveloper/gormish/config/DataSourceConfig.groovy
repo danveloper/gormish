903226c5c2cc9c2f6f570359d9315a30f9cc86a9
Dan Woods <dwoods@thig.com>
2012-07-18 18:18:53 -0400

    oops, reverting datasource to in-memory db

--- src/main/groovy/com/danveloper/gormish/config/DataSourceConfig.groovy
+++ src/main/groovy/com/danveloper/gormish/config/DataSourceConfig.groovy
@@ -6,12 +6,11 @@ package com.danveloper.gormish.config
 
 dataSource {
     pooled = true
-    driverClassName = "com.mysql.jdbc.Driver"
-    dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
-    username = "root"
+    driverClassName = "org.h2.Driver"
+    username = "sa"
     password = ""
     dbCreate = "create-drop"
-    url = "jdbc:mysql://localhost/gormish?useUnicode=yes&characterEncoding=UTF-8"
+    url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
 }
 hibernate {
     cache.use_second_level_cache = true
